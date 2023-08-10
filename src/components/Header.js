01aa881f64a23b28f223206e7c86984452c12ae1
Jeromeoui <zherong.teo@gmail.com>
2023-08-11 00:50:32 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,11 +15,11 @@ const pages = [
   {
     title: 'Pre-Requisites',
     url: '/prereqs',
-  }, 
+  },
   {
     title: 'Processes',
     url: '/processes',
-  }, 
+  },
   {
     title: 'FAQ',
     url: '/faq',
@@ -33,106 +33,109 @@ export default function SearchAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar 
+      <AppBar
         position="fixed"
         sx={{
-          backgroundColor: '#0d47a1', 
+          backgroundColor: '#0d47a1',
           zIndex: (theme) => theme.zIndex.drawer + 1,
           height: '56px',
         }} >
         <Toolbar>
-        <Grid 
+          <Grid
             container spacing={1}
             direction="row"
             justifyContent="space-between"
             alignItems="center">
-          
-        <Grid item xs={7} md={4}>
-          <Link to="/" style={{ textDecoration: 'none', color: 'white', display: 'flex',}}
-            sx={{
-              '&:hover': {
-                textDecoration: 'none',
-                color: 'blue'
-              }, 
-            }}>
-          <Grid container alignItems="center" spacing={1.5}>
-          <Grid item>
-            <HomeIcon style={{ marginTop: '-2px', }}
-                            sx={{ '&:hover': {
-                              textDecoration: 'none',
-                              color: '#D2E7FF'
-                            }, 
-                            '&:active': {
-                              textDecoration: 'none',
-                              color: '#5EA7FB'
-                            }, 
-                            }}/>
-          </Grid>
-          <Grid item>
-            <Typography
-                variant="h6"
-                style={{ marginTop: '-4px', fontSize: '20px' }}
-                noWrap component="div"
-                // style={{ marginLeft : isXlScreen ? '-100px ': '-200px' }}
-                sx={{ flexGrow: 1,
-                    '&:hover': {
-                      textDecoration: 'none',
-                      color: '#D2E7FF'
-                    }, 
-                    '&:active': {
-                      textDecoration: 'none',
-                      color: '#5EA7FB'
-                    }, 
-                  }}>
-                User Access Management (UAM)
-            </Typography>
+
+            <Grid item xs={7} md={4}>
+              <Link to="/" style={{ textDecoration: 'none', color: 'white', display: 'flex', }}
+                sx={{
+                  '&:hover': {
+                    textDecoration: 'none',
+                    color: 'blue'
+                  },
+                }}>
+                <Grid container alignItems="center" spacing={1.5}>
+                  <Grid item>
+                    <HomeIcon style={{ marginTop: '-2px', }}
+                      sx={{
+                        '&:hover': {
+                          textDecoration: 'none',
+                          color: '#D2E7FF'
+                        },
+                        '&:active': {
+                          textDecoration: 'none',
+                          color: '#5EA7FB'
+                        },
+                      }} />
+                  </Grid>
+                  <Grid item>
+                    <Typography
+                      variant="h6"
+                      style={{ marginTop: '-4px', fontSize: '20px' }}
+                      noWrap component="div"
+                      // style={{ marginLeft : isXlScreen ? '-100px ': '-200px' }}
+                      sx={{
+                        flexGrow: 1,
+                        '&:hover': {
+                          textDecoration: 'none',
+                          color: '#D2E7FF'
+                        },
+                        '&:active': {
+                          textDecoration: 'none',
+                          color: '#5EA7FB'
+                        },
+                      }}>
+                      User Access Management (UAM)
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Link>
             </Grid>
-            </Grid>
-          </Link>
-        </Grid>
-        {isWindowMaximized && (
-          <Grid item xs={3} md={4}>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex", lg: "flex" }}}>
-            {pages.map((page, index) => (
-              <Button
-                key={index}
-                sx={{ my: 2.4, mx: {md: 0, lg: 2}, color: "white", display: "block",
-                fontSize: {md: "15px", lg: "17px"},
-                marginTop: '16px', 
-                '&:hover': {
-                  textDecoration: 'none',
-                  color: '#D2E7FF'
-                }, 
-                '&:active': {
-                  textDecoration: 'none',
-                  color: '#5EA7FB'
-                }, 
-                }}
-                onClick={() => navigate(page.url)}
-              >
-                {page.title}
-              </Button>
-              // </NavLink>
-            ))}
-          </Box>
+            {isWindowMaximized && (
+              <Grid item xs={3} md={4}>
+                <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex", lg: "flex" } }}>
+                  {pages.map((page, index) => (
+                    <Button
+                      key={index}
+                      sx={{
+                        my: 2.4, mx: { md: 0, lg: 2 }, color: "white", display: "block",
+                        fontSize: { md: "15px", lg: "17px" },
+                        marginTop: '16px',
+                        '&:hover': {
+                          textDecoration: 'none',
+                          color: '#D2E7FF'
+                        },
+                        '&:active': {
+                          textDecoration: 'none',
+                          color: '#5EA7FB'
+                        },
+                      }}
+                      onClick={() => navigate(page.url)}
+                    >
+                      {page.title}
+                    </Button>
+                    // </NavLink>
+                  ))}
+                </Box>
+              </Grid>
+            )}
+            {isWindowMaximized && (
+              <div>
+                <Grid container alignItems="center" justifyContent="flex-end">
+                  <Grid item>
+                    <Link to="https://hiskcuivsweb01.healthgrp.com.sg/uam">
+                      <Button variant="contained"
+                        // component='a' href="https://hiskcuivsweb01.healthgrp.com.sg/uam/"
+                        style={{ marginRight: isXlScreen ? '25px ' : '15px' }}
+                      >Login
+                      </Button>
+                    </Link>
+                  </Grid>
+                </Grid>
+              </div>
+            )}
           </Grid>
-        )}
-        {isWindowMaximized && (
-        <div>
-          <Grid container alignItems="center" justifyContent="flex-end">
-          <Grid item>
-            <Link to="https://hiskcuivsweb01.healthgrp.com.sg/uam">
-              <Button variant="contained" 
-              // component='a' href="https://hiskcuivsweb01.healthgrp.com.sg/uam"
-              style={{ marginRight : isXlScreen ? '25px ': '15px' }}
-              >Login
-              </Button>
-            </Link>
-          </Grid>
-          </Grid>
-        </div>
-        )}
-        </Grid>
         </Toolbar>
       </AppBar>
     </Box>
